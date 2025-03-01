3b06bce377f7c140445895917a657b7aa61da6a1
Nitin <manashchoudhary123@gmail.com>
2025-03-01 08:05:13 +0530

    user project create

---
+++ src/main/java/com/user/Controller/UserController.java
@@ -0,0 +1,46 @@
+package com.user.Controller;
+
+import com.user.Entity.User;
+import com.user.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id).get();
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User updatedUser = userService.updateUser(id, userDetails);
+        return ResponseEntity.ok().body(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/user/Reposiraty/UserRepository.java
@@ -0,0 +1,9 @@
+package com.user.Reposiraty;
+
+import com.user.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
